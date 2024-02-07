1fedbb0147f7032308fc99fd86914915e6d096f8
Ahmed-ElAttary <ahmedelattary@gmail.com>
2024-02-07 15:20:57 +0200

    deployment 3

--- app/home/page.js
+++ app/home/page.js
@@ -3,24 +3,25 @@ import { GeoJsonDataSource, useCesium } from "resium";
 import Controls from "../components/HomeControls/Controls.jsx";
 import Markers from "../components/Markers/Markers.jsx";
 import ViewerContainer from "../components/ViewerContainer.component.jsx";
-import GeojsonComp from "../components/GeojsonComp/GeojsonComp.jsx"
+import { Color } from "cesium";
+// import GeojsonComp from "../components/GeojsonComp/GeojsonComp.jsx"
 
 
 export default function Home() {
 
   return (
     <ViewerContainer>
-            {/* <GeoJsonDataSource
+            <GeoJsonDataSource
          data= "/shapefile.geojson"
-clampToGround
-ref={geojsonRef}
+// clampToGround
+// ref={geojsonRef}
 
 stroke={Color.RED}
 strokeWidth={10}
-     fill={Color.WHEAT.withAlpha(0.8)}
+     fill={Color.WHEAT.withAlpha(0.3)}
    
-         /> */}
-         <GeojsonComp/>
+         />
+         {/* <GeojsonComp/> */}
       <Markers />
 
       <Controls />
