5d1b225b4d8b847f2cfe65ee35a39e7e08dad3c2
Ahmed-ElAttary <ahmedelattary@gmail.com>
2024-02-26 14:30:18 +0200

    fixing zooms issue

--- app/components/HomeControls/ZoomBtn/ZoomBtn.jsx
+++ app/components/HomeControls/ZoomBtn/ZoomBtn.jsx
@@ -1,21 +1,27 @@
 import React from "react";
 import { Button } from "primereact/button";
 import { useCesium } from "resium";
-import { Cartesian3 } from "cesium";
+import { Cartesian3, Cartographic } from "cesium";
 const ZoomBtn = () => {
   const viewerCs = useCesium();
+  const amount = () => {
+    const height = Cartographic.fromCartesian(
+      viewerCs.camera.positionWC
+    ).height;
+    return Math.min(50000, height * 0.5);
+  };
   return (
     <>
       <Button
         icon="pi pi-plus"
         onClick={() => {
-          viewerCs.camera.zoomIn();
+          viewerCs.camera.zoomIn(amount());
         }}
       />
       <Button
         icon="pi pi-minus"
         onClick={() => {
-          viewerCs.camera.zoomOut();
+          viewerCs.camera.zoomOut(amount());
         }}
       />
     </>
